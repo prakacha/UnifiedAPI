2c677bcc687303329406be0ed440226a9ef25b80
Prakash Chandra <chandra.prakash@tieto.com>
2018-10-24 16:26:40 +0530

    Signed-off-by: Prakash Chandra <chandra.prakash@tieto.com> Upload of UnifiedAPI automation to GIT 24-OCT-2018, 04:26 PM

---
+++ src/main/java/com/qa/client/RestClient.java
@@ -0,0 +1,34 @@
+//Unified API - RestClient.java
+package com.qa.client;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class RestClient {		
+// POST method to create a new customer in MDM
+// create closable-http-response method		
+	public CloseableHttpResponse post(String url, String entityString, HashMap<String, String> headerMap) throws ClientProtocolException, IOException{	// API url, paylod(that contains JSON file details), and headerMAP(e.g content type, userid etc)
+// creates HttpClients object, that will be responsible to execute the url		
+		CloseableHttpClient httpClient = HttpClients.createDefault();	
+		HttpPost httppost = new HttpPost(url);	//creates http post request for the given API url
+//Create Entity pay-load(JSON)		
+		httppost.setEntity(new StringEntity(entityString));	// setEntity method is used to define the payload, the data that we are going to pass through JSON		
+//create header
+		for (Map.Entry<String, String> entry : headerMap.entrySet()){
+			httppost.addHeader(entry.getKey(), entry.getValue());
+		}
+//hit API		
+		CloseableHttpResponse closeableHttpResponse = httpClient.execute(httppost);
+		return closeableHttpResponse;	
+		
+	}	//Method post	
+	
+} 	// class RestClient
---
+++ src/test/java/com/qa/tests/PostAPI_CreateNewCustomer.java
@@ -0,0 +1,233 @@
+//Unified API - PostAPICreateNewCustomer.java
+package com.qa.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import com.qa.base.TestBase;
+import com.qa.client.RestClient;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+
+public class PostAPI_CreateNewCustomer extends TestBase {
+	
+// declaration of class objects	
+	//user defined Class object
+	TestBase testBase;
+	String serviceUrl_CreateCustomer;
+	String serviceUrl_SearchCustomer;
+	String apiUrl_CreateCustomer;
+	String url_MasterAPI;
+	String url_Create;
+	String url_Search;
+	RestClient restClient;
+	CloseableHttpResponse closeableHttpResponse;
+	
+	//ExtentReport class object	
+	static ExtentReports extent_report;
+	static ExtentTest extent_test;
+
+// build API url	
+@BeforeMethod
+	public void setup() throws ClientProtocolException, IOException{
+		testBase = new TestBase();
+		url_MasterAPI = prop.getProperty("MasterAPI_URL");
+		serviceUrl_CreateCustomer = prop.getProperty("serviceURL_CreateCustomer");
+		serviceUrl_SearchCustomer = prop.getProperty("serviceURL_SearchCustomer");
+		url_Create = url_MasterAPI + serviceUrl_CreateCustomer;
+		url_Search = url_MasterAPI + serviceUrl_SearchCustomer;
+	}	//Method SetUp close
+
+
+// ExtentReport- startTest Method
+@BeforeTest
+	public static void startTest()
+	{
+		extent_report = new ExtentReports(System.getProperty("user.dir")+"\\ExecutionResults.html", false);
+		extent_report.addSystemInfo("Suite Description", "API calls to MDM system");
+		extent_report.loadConfig(new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\qa\\extentReport\\extent-config.xml"));
+	}
+	
+// POST API call to create customer
+@Test(priority =0)
+	public void test_Post_API_Create() throws ClientProtocolException, IOException {
+			System.out.println("TEST#1 POST API to create customer------------------------------------------------------------");
+			System.out.flush();
+			
+			extent_test = extent_report.startTest("test_Post_API_Create");
+			extent_test.log(LogStatus.INFO, "Test description: create customer using post API");
+			
+		//Create a hash map for passing header		
+			HashMap<String, String> headerMap = new HashMap<String, String>();
+			headerMap.put("Content-Type", "application/json");	// 'content header' as application/JSON
+			//headerMap.put("userName", "UserName_Value");		// pass value like user-name only if required
+			//headerMap.put("Password", "Password_Value");		// pass value like password only if required
+						
+		// Read data from JSON in the form of string 
+			String usersJSONString = "";
+	        try {
+	        	usersJSONString = new String(Files.readAllBytes( Paths.get(System.getProperty("user.dir")+"/src/main/java/com/qa/data/data_CreateCustomer.json")));
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        }
+	        
+	    //Convert String into JSON object 
+	        JSONObject responseJson = new JSONObject(usersJSONString);
+	
+	    //retrieve one level element from JSON
+	        Object strAgreementtype = responseJson.get("agreementType");
+	        System.out.println("Agreement type: "+strAgreementtype);
+	        extent_test.log(LogStatus.INFO, "AgreementType: "+strAgreementtype);
+	    //retrieve two level element
+	        System.out.println("Response with Initial firstName " + responseJson);
+	        JSONObject responseJson_basicCustomerInfo = responseJson.getJSONObject("basicCustomerInfo");
+	        responseJson_basicCustomerInfo.remove("firstName");
+	        System.out.println("Response without firstName " + responseJson);
+	        responseJson_basicCustomerInfo.put("firstName", "test_97");
+	        System.out.println("Response with final firstName " + responseJson);
+	        
+	    //retrieve two level array-element  
+	        JSONObject responseJson_CustomerContactInfo = responseJson.getJSONObject("customerContactInfo");
+	        JSONArray responseJson_Array_Contacts = responseJson_CustomerContactInfo.getJSONArray("contacts");
+	        System.out.println("Contacts Array item --- >"+responseJson_Array_Contacts.get(0));
+	        JSONObject responseJson_Contacts = responseJson_Array_Contacts.getJSONObject(0);
+	        JSONObject responseJson_Contact = responseJson_Contacts.getJSONObject("contact");
+	        System.out.println("Contact item1 --- >"+responseJson_Contact.get("value"));	//retrieve email address
+	        
+	    //Convert JSONObject back into String        
+	        usersJSONString = responseJson.toString();
+	
+	    // update JSON file with new details        
+	        try {
+	            Files.write(Paths.get(System.getProperty("user.dir")+"/src/main/java/com/qa/data/data_CreateCustomer.json"), usersJSONString.getBytes());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	       
+	    //POST the request
+			restClient = new RestClient();	
+			closeableHttpResponse = restClient.post(url_Create, usersJSONString, headerMap);		// hit API
+			
+		//Validate Status Code
+			int strStatusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+			System.out.println("Status code: "+strStatusCode);
+			Assert.assertEquals(strStatusCode, RESPONSE_STATUS_CODE_200);
+			
+		//Write result in Extent report			
+			if (strStatusCode == RESPONSE_STATUS_CODE_200){
+				extent_test.log(LogStatus.PASS, "Response Code: "+strStatusCode);
+			}
+			else {
+				extent_test.log(LogStatus.FAIL, "Response Code: "+strStatusCode);
+			}
+		//retrieve response string 			
+			String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(),"UTF-8" );	//return the response in string format
+			System.out.println("Response String from API: " + responseString);
+			extent_test.log(LogStatus.PASS, "Response String: "+ responseString);
+			
+		//Validate the Response
+			//Pia/Hannu to update more on this
+			
+		//endTest 	
+			extent_report.endTest(extent_test);
+			extent_report.flush();		
+							
+	} // Method postAPITest_Create 
+
+
+//POST API call to search customer
+@Test(priority =1)
+	public void test_Post_API_Search() throws ClientProtocolException, IOException {
+			System.out.println("");
+			System.out.println("");
+			System.out.println("TEST#2 POST API to search customer------------------------------------------------------------");
+			System.out.flush();
+			extent_test = extent_report.startTest("test_Post_API_Search");
+			extent_test.log(LogStatus.INFO, "Test description: search customer using post API");
+			
+		//Create a hash map for passing header		
+			HashMap<String, String> headerMap = new HashMap<String, String>();
+			headerMap.put("Content-Type", "application/json");	// 'content header' as application/JSON
+			//headerMap.put("userName", "UserName_Value");		// pass value like user-name only if required
+			//headerMap.put("Password", "Password_Value");		// pass value like password only if required
+					
+		//Read data from JSON in the form of string 
+			String usersJSONString = "";
+		    try {
+		    	usersJSONString = new String(Files.readAllBytes( Paths.get(System.getProperty("user.dir")+"/src/main/java/com/qa/data/data_SearchCustomer.json")));
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }	
+	
+		//Convert String into JSON object 
+	        JSONObject responseJson = new JSONObject(usersJSONString);	    
+		    
+		    
+	    //POST the request
+			restClient = new RestClient();	
+			closeableHttpResponse = restClient.post(url_Search, usersJSONString, headerMap);		// hit API
+	
+		//Validate Status Code
+			int strStatusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+			System.out.println("Status code: "+strStatusCode);
+			Assert.assertEquals(strStatusCode, RESPONSE_STATUS_CODE_200);
+	
+		//Write result in Extent report		
+			if (strStatusCode == RESPONSE_STATUS_CODE_200){
+				extent_test.log(LogStatus.PASS, "Response Code: "+strStatusCode);
+			}
+			else {
+				extent_test.log(LogStatus.FAIL, "Response Code: "+strStatusCode);
+			}
+			
+		//retrieve response string 			
+			String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(),"UTF-8" );	//return the response in string format
+			System.out.println("Response String from API: " + responseString);	
+		  //extent_test.log(LogStatus.FAIL, "Response String: "+responseString);
+		//endTest 
+			extent_report.endTest(extent_test);
+			extent_report.flush();		
+	} // Method postAPITest_Search
+
+
+
+//ExtentReport- getResult Method
+@AfterMethod
+	public void getResult(ITestResult result){
+			if(result.getStatus() == ITestResult.FAILURE){
+				extent_test.log(LogStatus.FAIL, "Summary --- '"+result.getName()+"' has failed");
+				extent_test.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+			}
+			else if(result.getStatus() == ITestResult.SKIP){
+				extent_test.log(LogStatus.SKIP, "Summary --- '"+result.getName()+"' is skipped");
+			}
+			else if(result.getStatus() == ITestResult.SUCCESS){
+				extent_test.log(LogStatus.PASS, "Summary --- '"+result.getName()+"' has passed");
+			}
+	}
+
+//ExtentReport- endTest Method
+@AfterTest
+	public void endTest() {
+		extent_report.endTest(extent_test);
+		extent_report.flush();
+	}
+
+} //Class PostAPI_CreateNewCustomer
+
